0f0f8bc7ccb8f37b45990f2a4060d12f26c91281
rberriob-1432 <rberriob@unicartagena.edu.co>
2026-09-08 21:18:07 -0500

    cambio de nombre query de contacto por id

--- src/main/java/aplicacion/servicios/dto/query/ConseguirContactoPorIdQuery.java
+++ src/main/java/aplicacion/servicios/dto/query/ConseguirContactoPorIdQuery.java
@@ -2,7 +2,7 @@ package aplicacion.servicios.dto.query;
 
 import jakarta.validation.constraints.NotBlank;
 
-public record ConseguirContactoPorId(
+public record ConseguirContactoPorIdQuery(
         @NotBlank(message = "El id no puede ser nulo")
         String id
 ) {
