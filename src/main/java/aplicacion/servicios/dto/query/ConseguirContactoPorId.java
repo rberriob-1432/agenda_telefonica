adf4f0ba27937c9064cf270ff4fac60eaf977cd0
rberriob-1432 <rberriob@unicartagena.edu.co>
2026-09-08 20:18:49 -0500

    Query

--- src/main/java/aplicacion/servicios/dto/query/ConseguirContactoPorId.java
+++ src/main/java/aplicacion/servicios/dto/query/ConseguirContactoPorId.java
@@ -1,4 +1,9 @@
 package aplicacion.servicios.dto.query;
 
-public class ConseguirContactoPorId {
+import jakarta.validation.constraints.NotBlank;
+
+public record ConseguirContactoPorId(
+        @NotBlank(message = "El id no puede ser nulo")
+        String id
+) {
 }
