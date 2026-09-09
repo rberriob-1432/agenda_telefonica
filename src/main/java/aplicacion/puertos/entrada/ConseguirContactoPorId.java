ef8ae4b79a2c84101c8ec85f8c69c222151e44f1
rberriob-1432 <rberriob@unicartagena.edu.co>
2026-09-08 21:27:29 -0500

    Correcion puerto

--- src/main/java/aplicacion/puertos/entrada/ConseguirContactoPorId.java
+++ src/main/java/aplicacion/puertos/entrada/ConseguirContactoPorId.java
@@ -1,8 +1,9 @@
 package aplicacion.puertos.entrada;
+import aplicacion.servicios.dto.query.ConseguirContactoPorIdQuery;
 import dominio.modelo.Contacto;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
 
 public interface ConseguirContactoPorId {
-    Contacto execute(@NotNull @Valid ConseguirContactoPorId query);
+    Contacto execute(@NotNull @Valid ConseguirContactoPorIdQuery query);
 }
