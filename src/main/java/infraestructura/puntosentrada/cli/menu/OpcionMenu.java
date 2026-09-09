03ca492e7ab83e616c4f8a267f638b916940f8dc
rberriob-1432 <rberriob@unicartagena.edu.co>
2026-09-09 10:15:50 -0500

    Modificacion de archivos de configuracion y opciones de menu

--- src/main/java/infraestructura/puntosentrada/cli/menu/OpcionMenu.java
+++ src/main/java/infraestructura/puntosentrada/cli/menu/OpcionMenu.java
@@ -1,4 +1,32 @@
 package infraestructura.puntosentrada.cli.menu;
 
-public class OpcionMenu {
+import java.util.Optional;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum OpcionMenu {
+
+    LISTAR_CONTACTOS(1, "Listar todos los contactos"),
+    BUSCAR_CONTACTO(2, "Buscar contacto por ID"),
+    CREAR_CONTACTO(3, "Crear contacto"),
+    ACTUALIZAR_CONTACTO(4, "Actualizar contacto"),
+    ELIMINAR_CONTACTO(5, "Eliminar contacto"),
+    SALIR(0, "Salir");
+
+    private final int numero;
+    private final String descripcion;
+
+    public static Optional<OpcionMenu> desdeNumero(
+            final int numero) {
+
+        for (final OpcionMenu opcion : values()) {
+            if (opcion.numero == numero) {
+                return Optional.of(opcion);
+            }
+        }
+
+        return Optional.empty();
+    }
 }
